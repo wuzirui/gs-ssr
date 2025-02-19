f8151eca3dc29b8b0d16bbaaf01a7fe8147813a1
wuzirui <wuzirui@tztyun.org>
2025-02-19 18:15:39 +0800

    add video

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bulma/css/bulma.min.css';
 import React from 'react';
 import Abstract from './components/Abstract';
 import Citation from './components/Citation';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
-import ImageCarousel from './components/ImageCarousel';
 import Poster from './components/Poster';
 import TeaserVideo from './components/TeaserVideo';
 import VideoCarousel from './components/VideoCarousel';
 import VideoPresentation from './components/VideoPresentation';
-
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bulma/css/bulma.min.css';
 import './styles/index.css';
 
 function App() {
@@ -19,7 +17,6 @@ function App() {
             <Hero />
             <TeaserVideo />
             <Abstract />
-            <ImageCarousel />
             <VideoPresentation />
             <VideoCarousel />
             <Poster />
--- src/components/VideoPresentation.jsx
+++ src/components/VideoPresentation.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 
 const VideoPresentation = () => {
   return (
-    <section className="hero is-small is-light">
+    <section className="hero is-small">
       <div className="hero-body">
-        <div className="container">
-          <h2 className="title is-3">Video Presentation</h2>
-          <div style={{ background: '#f5f5f5', padding: '20px', textAlign: 'center' }}>
-            Video Presentation Coming Soon
+        <div className="container is-max-desktop">
+          <div className="has-text-centered" style={{ marginTop: '1rem', marginBottom: '4rem' }}>
+            <h2 className="title is-3">Video Presentation</h2>
+            <div style={{ height: '1rem' }}></div>
+            <div className="columns is-centered">
+              <div className="column">
+                <div className="publication-video">
+                  <iframe 
+                    src="https://www.youtube.com/embed/hdyaAKl7zzE" 
+                    title="Project Video"
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
